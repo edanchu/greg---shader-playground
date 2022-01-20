96810c31070e1c1b82284661634b6afcab0e38f6
edanchu <91294716+edanchu@users.noreply.github.com>
2022-01-20 13:24:54 -0800

    fullscreen quad with custom shadermat

--- client/src/pages/graphics_test.js
+++ client/src/pages/graphics_test.js
@@ -1,120 +1,66 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import * as THREE from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-const style = {
-    height: 500 // we can control scene size by setting container dimensions
-};
-
-class TestScene extends Component {
+class GraphicsTest extends Component {
     componentDidMount() {
         this.sceneSetup();
-        this.addCustomSceneObjects();
         this.startAnimationLoop();
-        window.addEventListener('resize', this.handleWindowResize);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this.handleWindowResize);
         window.cancelAnimationFrame(this.requestID);
-        this.controls.dispose();
     }
 
-    // Standard scene setup in Three.js. Check "Creating a scene" manual for more information
-    // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
-    sceneSetup = () => {
-        // get container dimensions and use them for scene sizing
-        const width = this.mount.clientWidth;
-        const height = this.mount.clientHeight;
+    sceneSetup(){
+        const width = 720;
+        const height = 480;
 
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(
-            75, // fov = field of view
-            width / height, // aspect ratio
-            0.1, // near plane
-            1000 // far plane
-        );
-        this.camera.position.z = 9; // is used here to set some distance from a cube that is located at z = 0
-        // OrbitControls allow a camera to orbit around the object
-        // https://threejs.org/docs/#examples/controls/OrbitControls
-        this.controls = new OrbitControls( this.camera, this.mount );
+        this.camera = new THREE.OrthographicCamera();
+        this.camera.position.z = 1;
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( width, height );
-        this.mount.appendChild( this.renderer.domElement ); // mount using React ref
+        this.mount.appendChild( this.renderer.domElement );
+
+        const planeGeometry = new THREE.PlaneGeometry(2,2)
+        const planeMaterial = new THREE.RawShaderMaterial({
+            uniforms:{},
+            vertexShader: this.getVertexShader(),
+            fragmentShader: this.getFragmentShader(),
+        });
+
+        this.scene.add(new THREE.Mesh(planeGeometry, planeMaterial));
     };
 
-    // Here should come custom code.
-    // Code below is taken from Three.js BoxGeometry example
-    // https://threejs.org/docs/#api/en/geometries/BoxGeometry
-    addCustomSceneObjects = () => {
-        const geometry = new THREE.BoxGeometry(2, 2, 2);
-        const material = new THREE.MeshPhongMaterial( {
-            color: 0x156289,
-            emissive: 0x072534,
-            side: THREE.DoubleSide,
-            flatShading: true
-        } );
-        this.cube = new THREE.Mesh( geometry, material );
-        this.scene.add( this.cube );
-
-        const lights = [];
-        lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-        lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-        lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-
-        lights[ 0 ].position.set( 0, 200, 0 );
-        lights[ 1 ].position.set( 100, 200, 100 );
-        lights[ 2 ].position.set( - 100, - 200, - 100 );
-
-        this.scene.add( lights[ 0 ] );
-        this.scene.add( lights[ 1 ] );
-        this.scene.add( lights[ 2 ] );
-    };
-
-    startAnimationLoop = () => {
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
-
+    startAnimationLoop(){
         this.renderer.render( this.scene, this.camera );
 
-        // The window.requestAnimationFrame() method tells the browser that you wish to perform
-        // an animation and requests that the browser call a specified function
-        // to update an animation before the next repaint
         this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
     };
 
-    handleWindowResize = () => {
-        const width = this.mount.clientWidth;
-        const height = this.mount.clientHeight;
+    getVertexShader(){
+        return `
+            attribute vec3 position;
 
-        this.renderer.setSize( width, height );
-        this.camera.aspect = width / height;
-
-        // Note that after making changes to most of camera properties you have to call
-        // .updateProjectionMatrix for the changes to take effect.
-        this.camera.updateProjectionMatrix();
-    };
-
-    render() {
-        return <div style={style} ref={ref => (this.mount = ref)} />;
+            void main(){
+                gl_Position = vec4(position, 1.0);
+            }
+        
+        `
     }
-}
 
-class GraphicsTest extends React.Component {
-    state = {isMounted: true};
+    getFragmentShader(){
+        return `
+            void main(){
+                gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+            }
+        
+        `
+    }
 
     render() {
-        const {isMounted = true} = this.state;
-        return (
-            <>
-                <button onClick={() => this.setState(state => ({isMounted: !state.isMounted}))}>
-                    {isMounted ? "Unmount" : "Mount"}
-                </button>
-                {isMounted && <TestScene />}
-                {isMounted && <div>Scroll to zoom, drag to rotate</div>}
-            </>
-        )
+        return <div ref={ref => (this.mount = ref)} />;
     }
 }
 
