23c42c33bd8eb85c0abb78109693fbcc0e7763e9
edanchu <91294716+edanchu@users.noreply.github.com>
2022-01-21 13:52:56 -0800

    added basic previous render passes
    3 buffers passed in to final shader as textures. todo: let buffer2 use buffer 1 and buffer 3 use buffers 1 and 2

--- client/src/pages/graphics_test.js
+++ client/src/pages/graphics_test.js
@@ -16,47 +16,112 @@ class GraphicsTest extends Component {
     }
 
     sceneSetup(){
-        const height = 720;
-        const width = height * 16/9;
+        this.height = 720;
+        this.width = this.height * 16/9;
 
         this.scene = new THREE.Scene();
         this.camera = new THREE.OrthographicCamera();
         this.camera.position.z = 1;
         this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize( width, height );
+        this.renderer.setSize( this.width, this.height );
         this.mount.appendChild( this.renderer.domElement );
 
         this.clock = new THREE.Clock(true);
 
-        let planeGeometry = new THREE.PlaneGeometry(2,2)
-        let planeMaterial = new THREE.RawShaderMaterial({
-            uniforms:{
-                iTime: {value: 0.0},
-                iDeltaTime: {value: 0.0},
-                iFrame: {value: 0},
-            },
-            vertexShader: this.getVertexShader(),
-            fragmentShader: this.getFragmentShader(),
-        });
+        this.createRenderBuffers();
 
-        this.quad = new THREE.Mesh(planeGeometry, planeMaterial);
+        this.planeGeometry = new THREE.PlaneGeometry(2,2)
+        this.createMaterials();
+
+        this.quad = new THREE.Mesh(this.planeGeometry, this.finalMat);
 
         this.scene.add(this.quad);
     };
 
     renderLoop(){
+
+        this.updateBufferUniforms();
+
+        this.renderer.setRenderTarget(this.renderTarget1);
+        this.quad.material = this.bufferMat1;
         this.renderer.render( this.scene, this.camera );
 
-        this.updateUniforms();
+        this.renderer.setRenderTarget(this.renderTarget2);
+        this.quad.material = this.bufferMat2;
+        this.renderer.render( this.scene, this.camera );
+
+        this.renderer.setRenderTarget(this.renderTarget3);
+        this.quad.material = this.bufferMat3;
+        this.renderer.render( this.scene, this.camera );
+
+        this.updateFinalUniforms();
+
+        this.renderer.setRenderTarget(null);
+        this.quad.material = this.bufferMat1;
+        this.renderer.render( this.scene, this.camera );
 
         this.requestID = window.requestAnimationFrame(this.renderLoop);
     };
 
-    updateUniforms(){
-        this.quad.material.uniforms.iDeltaTime.value = this.clock.getDelta();
-        this.quad.material.uniforms.iTime.value = this.clock.getElapsedTime();
-        this.quad.material.uniforms.iFrame.value += 1;
+    createRenderBuffers(){
+        this.renderTarget1 = new THREE.WebGLRenderTarget(this.width, this.height);
+        this.renderTarget2 = new THREE.WebGLRenderTarget(this.width, this.height);
+        this.renderTarget3 = new THREE.WebGLRenderTarget(this.width, this.height);
+
+        this.renderTarget1.texture.wrapS = this.renderTarget1.texture.wrapT = THREE.RepeatWrapping;
+        this.renderTarget2.texture.wrapS = this.renderTarget2.texture.wrapT = THREE.RepeatWrapping;
+        this.renderTarget3.texture.wrapS = this.renderTarget3.texture.wrapT = THREE.RepeatWrapping;
+
+        this.renderTarget1.texture.magFilter = this.renderTarget1.texture.minFilter = THREE.NearestFilter;
+        this.renderTarget2.texture.magFilter = this.renderTarget2.texture.minFilter = THREE.NearestFilter;
+        this.renderTarget3.texture.magFilter = this.renderTarget3.texture.minFilter = THREE.NearestFilter;
+
+        this.renderTarget1.texture.depthBuffer = false;
+        this.renderTarget2.texture.depthBuffer = false;
+        this.renderTarget3.texture.depthBuffer = false;
     }
+
+    updateFinalUniforms(){
+        this.finalMat.uniforms.iDeltaTime.value = this.bufferMat1.uniforms.iDeltaTime.value;
+        this.finalMat.uniforms.iTime.value = this.bufferMat1.uniforms.iTime.value;
+        this.finalMat.uniforms.iFrame.value += 1;
+    }
+
+    updateBufferUniforms(){
+        this.bufferMat1.uniforms.iDeltaTime.value = this.bufferMat2.uniforms.iDeltaTime.value = this.bufferMat3.uniforms.iDeltaTime.value = this.clock.getDelta();
+        this.bufferMat1.uniforms.iTime.value = this.bufferMat2.uniforms.iTime.value = this.bufferMat3.uniforms.iTime.value = this.clock.getElapsedTime();
+        this.bufferMat1.uniforms.iFrame.value = this.bufferMat2.uniforms.iFrame.value = this.bufferMat3.uniforms.iFrame.value += 1;
+    }
+
+    createMaterials(){
+        this.finalMat = new THREE.RawShaderMaterial({uniforms:{
+            iTime: {value: 0.0},
+            iDeltaTime: {value: 0.0},
+            iFrame: {value: 0},
+            iBufferTexture1: {value: this.renderTarget1.texture},
+            iBufferTexture2: {value: this.renderTarget2.texture},
+            iBufferTexture3: {value: this.renderTarget3.texture}
+        }, vertexShader: this.getVertexShader(), fragmentShader: this.getFinalFragmentShader()});
+
+        this.bufferMat1 = new THREE.RawShaderMaterial({uniforms:{
+            iTime: {value: 0.0},
+            iDeltaTime: {value: 0.0},
+            iFrame: {value: 0},
+        }, vertexShader: this.getVertexShader(), fragmentShader: this.getBuffer1FragShader()});
+
+        this.bufferMat2 = new THREE.RawShaderMaterial({uniforms:{
+            iTime: {value: 0.0},
+            iDeltaTime: {value: 0.0},
+            iFrame: {value: 0},
+        }, vertexShader: this.getVertexShader(), fragmentShader: this.getBuffer2FragShader()});
+
+        this.bufferMat3 = new THREE.RawShaderMaterial({uniforms:{
+            iTime: {value: 0.0},
+            iDeltaTime: {value: 0.0},
+            iFrame: {value: 0},
+        }, vertexShader: this.getVertexShader(), fragmentShader: this.getBuffer3FragShader()});
+    }
+
 
     getVertexShader(){
         return `
@@ -69,25 +134,30 @@ class GraphicsTest extends Component {
         `;
     }
 
-    getFragShaderCustomCode(){
+    getFinalFragShaderCustomCode(){
         return `
 
         void mainImage(out vec4 FragColor, in vec4 FragCoord)
         {
-            FragColor = vec4(iTime, 0.0, 1.0, 1.0);
+            vec4 buff1 = texture2D(iBufferTexture1, FragCoord.xy);
+            FragColor = buff1;
         }
 
         `;
     }
 
-    getFragmentShader(){
+    getFinalFragmentShader(){
         return `
         precision mediump float;
 
         uniform float iTime;
         uniform float iDeltaTime;
+        uniform int iFrame;
+        uniform sampler2D iBufferTexture1;
+        uniform sampler2D iBufferTexture2;
+        uniform sampler2D iBufferTexture3;
 
-        ` + this.getFragShaderCustomCode() + `
+        ` + this.getFinalFragShaderCustomCode() + `
 
         void main(){
             mainImage(gl_FragColor, gl_FragCoord);
@@ -95,6 +165,91 @@ class GraphicsTest extends Component {
     
     `;
     }
+
+    getBuffer1FragShader(){
+        return `
+        precision mediump float;
+
+        uniform float iTime;
+        uniform float iDeltaTime;
+        uniform int iFrame;
+
+        ` + this.getBuffer1FragShaderCustomCode() + `
+
+        void main(){
+            mainImage(gl_FragColor, gl_FragCoord);
+        }
+    
+    `;
+    }
+
+    getBuffer2FragShader(){
+        return `
+        precision mediump float;
+
+        uniform float iTime;
+        uniform float iDeltaTime;
+        uniform int iFrame;
+
+        ` + this.getBuffer2FragShaderCustomCode() + `
+
+        void main(){
+            mainImage(gl_FragColor, gl_FragCoord);
+        }
+    
+    `;
+    }
+
+    getBuffer3FragShader(){
+        return `
+        precision mediump float;
+
+        uniform float iTime;
+        uniform float iDeltaTime;
+        uniform int iFrame;
+
+        ` + this.getBuffer3FragShaderCustomCode() + `
+
+        void main(){
+            mainImage(gl_FragColor, gl_FragCoord);
+        }
+    
+    `;
+    }
+
+    getBuffer1FragShaderCustomCode(){
+        return `
+
+        void mainImage(out vec4 FragColor, in vec4 FragCoord)
+        {
+            FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+
+        `;
+    }
+
+    getBuffer2FragShaderCustomCode(){
+        return `
+
+        void mainImage(out vec4 FragColor, in vec4 FragCoord)
+        {
+            FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+        }
+
+        `;
+    }
+
+    getBuffer3FragShaderCustomCode(){
+        return `
+
+        void mainImage(out vec4 FragColor, in vec4 FragCoord)
+        {
+            FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+        }
+
+        `;
+    }
+
 
     render() {
         return <div ref={ref => (this.mount = ref)} />;
