55fe27b5d3e086ff7725f27b3e2863d43939a8b0
RafiHartounian <Rafihartounian@gmail.com>
2022-01-21 10:53:54 -0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import GraphicsTest from './pages/graphics_test'
+import Example from './pages/page_test'
 import './App.css';
 
 function App() {
   return (
-    <GraphicsTest />
+    <Example />
   );
 }
 
---
+++ client/src/pages/page_test.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+function Example() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+    const [fruit, setFruit] = useState('');
+  
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me to raise count
+        </button>
+        <p>Fruit: {fruit}</p>
+        <button onClick={() => setFruit('apple')}>
+          Apple
+        </button>
+        <button onClick={() => setFruit('cherry')}>
+          Cherry
+        </button>
+        <button onClick={() => setFruit('banana')}>
+          Banana
+        </button>
+      </div>
+    );
+  }
+
+  export default Example
