8aeeb37708915829d7d327bb58f0ef816f850ac3
RafiHartounian <Rafihartounian@gmail.com>
2022-01-21 11:55:26 -0800

    Update page_test.js

--- client/src/pages/page_test.js
+++ client/src/pages/page_test.js
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Example() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
     const [fruit, setFruit] = useState('');
   
+    useEffect(() => {
+      document.title = fruit;
+    });
+
     return (
       <div>
         <p>You clicked {count} times</p>
