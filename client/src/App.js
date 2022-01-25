8ca9a40cb31f04f39bdab834e9de8c352a171236
RafiHartounian <Rafihartounian@gmail.com>
2022-01-25 10:42:30 -0800

    Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import GraphicsComponent from './components/graphics_component'
-import Example from './pages/page_test'
 import './App.css';
+import Home from './pages/home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   const displayHeight = 480;
   return (
-    <div>
-      <GraphicsComponent height={displayHeight}  />
-      <Example />
-    </div>
+    /*<><div>
+      <GraphicsComponent height={displayHeight} />
+    </div><>*/
+    <>
+        <Router>
+          <Home />
+          <Routes>
+            <Route path='/' exact />
+          </Routes>
+        </Router>
+      </>
   );
 }
 
