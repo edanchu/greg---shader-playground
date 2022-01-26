c93d475df6673262e83cc0401a4baf8850682df0
HaikDivanyan <70823905+HaikDivanyan@users.noreply.github.com>
2022-01-26 15:49:28 -0800

    Added CRUD API for Project

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProjectSchema = new mongoose.Schema({
   owner: { type: mongoose.Schema.Types.ObjectId, required: true },
   title: { type: String, required: true },
-  description: { type: String, default: '', required: true },
-  code: { type: String, default: '', required: true },
+  description: { type: String, default: "", required: true },
+  code: { type: String, default: "", required: true },
   likes: { type: Number, default: 0, required: true },
   public: { type: Boolean, default: false, required: true },
-  textures: { type: [String], required: true },
+  //textures: { type: [String], required: true },
+  textures: { type: [String], required: false },
 });
 
-module.exports = mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model("Project", ProjectSchema);
