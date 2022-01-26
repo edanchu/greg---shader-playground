cc79abc48b2872e1708dc29c2de5b8f861f03546
RafiHartounian <Rafihartounian@gmail.com>
2022-01-25 18:44:17 -0800

    Home/Browse

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Browse from './pages/browse';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
     <Router>
+      <Navbar />
       <Routes>
         <Route path= '/' element = {<Home/>}/>
         <Route path= '/Browse' element = {<Browse/>}/>
