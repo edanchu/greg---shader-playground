d096a8dbe00a5dd764481a039a345ec1f25cfa09
gevorgakopyan <gevorgakopyan01@gmail.com>
2022-01-25 16:09:17 -0800

    Browse

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import GraphicsTest from './pages/graphics_test'
 import Example from './pages/page_test'
 import './App.css';
+import Home from './pages/home';
+import Browse from './pages/browse';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Example />
+    <>
+    <Router>
+      <Routes>
+        <Route path= '/' element = {<Home/>}/>
+        <Route path= '/Browse' element = {<Browse/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
