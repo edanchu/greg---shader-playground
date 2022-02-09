ed14740f293ebd1623a25449684cc89a06799743
gevorgakopyan <gevorgakopyan01@gmail.com>
2022-02-08 17:31:22 -0800

    Neon logo
    Added glowing logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/Browse'
+                to='/Cards'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
