7adcbd44531d88338ba8e43cfee80e83ab69e67b
RafiHartounian <Rafihartounian@gmail.com>
2022-02-09 21:29:26 -0800

    Login Update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import '../App.css';
-import { ButtonSignUp } from '../components/ButtonSignUp.js';
-import { ButtonBrowse } from '../components/ButtonBrowse.js';
-function Login() {
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import { Link } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+import "../App.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <>
-    <h1>Login</h1>
-    <p></p>
-    <div className='hero-btns'>
-      <ButtonSignUp
-        className='btns'
-        buttonStyle='btn--outline'
-        buttonSize='btn--large'
-      >
-        Create an Account
-      </ButtonSignUp>
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <h className="header"> Email </h>
+        <Form.Group controlId="email" className="input-box">
+          <Form.Control
+            className="input-box2"
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <h className="header"> Password </h>
+        <Form.Group controlId="password" className="input-box">
+          <Form.Control
+            className="input-box2"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+          <Link to='/Account' className='login-link'>
+            <Button className='login-button' type="submit" disabled={!validateForm()}>
+              Login
+            </Button>
+          </Link> 
+          <Link to='/Sign-Up' className='login-link'>
+            <Button className='SignUp-button' type="submit">
+              Don't have an account? Sign up!
+            </Button>
+          </Link> 
+      </Form>
     </div>
-    </>
   );
-
 }
-
-export default Login;
-
