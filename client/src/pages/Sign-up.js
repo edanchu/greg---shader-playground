e2157d296de8370d38b80d2ba075698ddb058948
RafiHartounian <Rafihartounian@gmail.com>
2022-02-17 20:25:39 -0800

    Connected Sign-up front-end to back-end

--- client/src/pages/Sign-up.js
+++ client/src/pages/Sign-up.js
@@ -9,23 +9,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 export default function SignUp() {
-<<<<<<< HEAD
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-=======
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
->>>>>>> 0b935b66a7614389c475b910b884550b8f86fdd4
 
 
   let navigate = useNavigate();
 
   function validateForm() {
     if (email.length <= 0 || password.length <= 0) {
-<<<<<<< HEAD
       toast.error("Email and Password must be at least 1 character");
     }
 
@@ -35,15 +28,6 @@ export default function SignUp() {
     
     else if (password !== confirmPassword) {
       toast.error("Passwords must match");
-=======
-      toast.error('Email and Password must be at least 1 character');
-    } else if (confirmPassword.length <= 0) {
-      toast.error('Please confirm password');
-    } else if (password !== confirmPassword) {
-      toast.error('Passwords must match');
-    } else {
-      return navigate('/UserPage');
->>>>>>> 0b935b66a7614389c475b910b884550b8f86fdd4
     }
   }
 
@@ -91,7 +75,6 @@ export default function SignUp() {
             onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
-<<<<<<< HEAD
         <h className="header"> Username </h>
         <Form.Group controlId="email" className="input-box">
           <Form.Control
@@ -104,10 +87,6 @@ export default function SignUp() {
         </Form.Group>
         <h className="header"> Password </h>
         <Form.Group controlId="password" className="input-box">
-=======
-        <h className='header'> Password </h>
-        <Form.Group controlId='password' className='input-box'>
->>>>>>> 0b935b66a7614389c475b910b884550b8f86fdd4
           <Form.Control
             className='input-box2'
             type='password'
@@ -124,7 +103,6 @@ export default function SignUp() {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </Form.Group>
-<<<<<<< HEAD
             <Button className='login-button' type='submit'>
               Create Account
             </Button>
@@ -134,17 +112,6 @@ export default function SignUp() {
               Have an account? Sign in! 
             </Button>
           </Link>
-=======
-        <Button className='login-button' onClick={() => validateForm()}>
-          Create Account
-        </Button>
-        <ToastContainer />
-        <Link to='/Login' className='login-link'>
-          <Button className='SignUp-button' type='submit'>
-            Have an account? Sign in!
-          </Button>
-        </Link>
->>>>>>> 0b935b66a7614389c475b910b884550b8f86fdd4
       </Form>
     </div>
   );
