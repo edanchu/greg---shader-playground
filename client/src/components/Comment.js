478aa21ec635afa9af2e23eb7a8a755045f911e8
Ethan Jackson <ethantjackson44@gmail.com>
2022-03-09 00:51:44 -0800

    fix like functionality for comments

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,15 +1,37 @@
-import axios from "axios";
-import React from "react";
-import { Col, Row, Figure, Button } from "react-bootstrap";
-import { toast } from "react-toastify";
-import './CommentSection.css'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Col, Row, Figure, Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import './CommentSection.css';
 
 function Comment(props) {
+  const [comment, setComment] = useState(props.comment);
+  const [liked, setLiked] = useState(
+    props.comment.likes.includes(props.user?._id)
+  );
+
   function handleLike() {
     if (!props.user) {
       toast.error('Must be signed in to like');
     } else {
-      axios.put("/api/user/like-comment/" + props.comment._id);
+      let newLikes = comment.likes;
+      let likeStatus;
+      if (comment.likes.indexOf(props.user._id) === -1) {
+        newLikes.push(props.user._id);
+        likeStatus = true;
+      } else {
+        newLikes = newLikes.filter((id) => id !== props.user._id);
+        likeStatus = false;
+      }
+      axios
+        .put('/api/user/update-comment/' + comment._id, {
+          ...comment,
+          likes: newLikes,
+        })
+        .then((res) => {
+          setComment({ ...comment, likes: newLikes });
+          setLiked(likeStatus);
+        });
     }
   }
 
@@ -17,25 +39,25 @@ function Comment(props) {
     <>
       <Row className='comment-existing-container'>
         <Col xs={'auto'}>
-          <h4>{props.comment.ownerName}</h4>
+          <h4>{comment.ownerName}</h4>
           <Figure.Image
-            src={"/avatars/" + props.comment.ownerAvatar}
+            src={'/avatars/' + comment.ownerAvatar}
             width={60}
             height={60}
           />
         </Col>
         <Col xs={'auto'}>
           <textarea
-            type="text"
-            value={props.comment.content}
+            type='text'
+            value={comment.content}
             className='comment-existing'
             disabled
             style={{ resize: 'none', marginTop: '1rem' }}
           />
         </Col>
         <Col>
-          <Button style={{ float: 'right' }}>
-            <i className="fas fa-thumbs-up" onClick={handleLike} />  {props.comment.likes.length}
+          <Button style={{ float: 'right' }} onClick={handleLike}>
+            <i className='fas fa-thumbs-up' /> {comment.likes.length}
           </Button>
         </Col>
       </Row>
