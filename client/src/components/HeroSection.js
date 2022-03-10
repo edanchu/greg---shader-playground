e171dd35c4c57d8a61ec5b05e882dc047f169868
edanchu <91294716+edanchu@users.noreply.github.com>
2022-03-10 00:12:02 -0800

    changed 'Get Started' button to link to editor page instead of login/logout

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import '../App.css';
-import { ButtonLogin } from './ButtonLogin';
 import './HeroSection.css';
 import { ButtonBrowse } from './ButtonBrowse';
+import '../pages/button.css';
+import { useNavigate } from 'react-router-dom';
 
 function HeroSection() {
+  let navigate = useNavigate();
+
+  const onClick = () => {
+    return navigate('/Editor');
+  };
+
   return (
     <div className='hero-container'>
       <video src='/videos/test.mp4' autoPlay loop muted />
       <h1 className='title'>SHADERS AWAIT!!!</h1>
       <div className='hero-btns'>
-        <ButtonLogin
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+        <button
+          className='btns btn--outline btn--large btn btn-mobile'
+          onClick={onClick}
         >
           GET STARTED
-        </ButtonLogin>
+        </button>
         <ButtonBrowse
           className='btns'
           buttonStyle='btn--primary'
