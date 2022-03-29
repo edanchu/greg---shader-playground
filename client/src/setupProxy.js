f54f5340243469e408bebf7036552b6e16db03be
Ethan Jackson <ethantjackson44@gmail.com>
2022-03-29 15:22:49 -0700

    update proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('https://edanchu.github.io/greg/api', {
+    createProxyMiddleware('/greg/api', {
       //target: 'http://localhost:8888',
       target: 'https://g-r-e-g.herokuapp.com',
     })
