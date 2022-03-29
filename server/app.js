ace927932be38befd0439d9bdaddeaf2aa6a275c
Ethan Jackson <ethantjackson44@gmail.com>
2022-03-29 15:00:14 -0700

    add app .js

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ mongoose.connect(
 const PORT = process.env.PORT || 8888;
 
 const userRouter = require('./routes/User');
-app.use('/api/user', userRouter);
+app.use('/greg/api/user', userRouter);
 
 app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
